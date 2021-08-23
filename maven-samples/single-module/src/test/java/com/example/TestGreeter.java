0a0c594bc283a0be5f962334e29a1af01d90d815
Tanzeel Alam <tanzeelalam@gmail.com>
2021-08-23 07:38:03 +0530

    Corrected grammar in test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, Thank you makes me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
